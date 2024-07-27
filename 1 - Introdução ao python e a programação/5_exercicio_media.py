88a6561aca3da1cb24e7ba702f7cc20501dd7231
Tiago Felipe <tiagosantos0412@gmail.com>
2024-07-27 12:38:59 -0300

    Exercício Operadores aritméticos Média Escolar.

--- 1 - Introdução ao python e a programação/5_exercicio_media.py
+++ 1 - Introdução ao python e a programação/5_exercicio_media.py
@@ -1,8 +1,12 @@
 #Escreva um programa onde o usuário digite duas notas e ele mostre a média das duas notas
 
-nota1 = float(input('Digite a primeira nota: '))
-nota2 = float(input('Digite a segunda nota: '))
+print('Média Escolar')
+nome = input('Insira o nome do aluno: ')
+nota_1 = float(input('Digite a primeira nota do aluno: '))
+nota_2 = float(input('Digite a segunda nota do aluno: '))
+nota_3 = float(input('Digite a terceira nota do aluno: '))
+nota_4 = float(input('Digite a quarta nota do aluno: '))
 
-media = (nota1 + nota2) / 2
+media = int((nota_1 + nota_2 + nota_3 + nota_4) / 4)
 
-print(media)
+print(f'A média do aluno {nome} é: {media}.')
