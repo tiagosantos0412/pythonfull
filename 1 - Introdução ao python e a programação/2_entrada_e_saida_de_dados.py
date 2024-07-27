112ed2f941875e36cb881111b0538f87b1257caa
Tiago Felipe <tiagosantos0412@gmail.com>
2024-07-27 11:45:14 -0300

    Entrada e saída de dados.

--- 1 - Introdução ao python e a programação/2_entrada_e_saida_de_dados.py
+++ 1 - Introdução ao python e a programação/2_entrada_e_saida_de_dados.py
@@ -3,9 +3,11 @@
 idade = 37
 resultado = f'Olá {nome}, sua idade é: {idade} anos'
 """
+# nome = 'Tiago Felipe dos Santos'
+# idade = 37
+
+# print(f'Olá {nome}, você tem {idade} anos.')
+
 nome = input('Digite o seu nome: ')
-idade = int(input('Digite sua idade: '))
-
-resultado = f'Bem vindo {nome}, você tem {idade} anos.'
-
-print(resultado) 
+idade  = input('Dgite a sua idade: ')
+print(f'Olá {nome}, você tem {idade} anos.')
