c5575b115226da5017cb0a85aa11305192aa6161
Tiago Felipe <tiagosantos0412@gmail.com>
2024-07-27 12:04:13 -0300

    Conversão de dados.

--- 1 - Introdução ao python e a programação/3_conversao_de_dados.py
+++ 1 - Introdução ao python e a programação/3_conversao_de_dados.py
@@ -1,2 +1,21 @@
 #Conversão de dados
 
+idade = '37'
+print(f'Valor antes da conversão: {idade} do tipo ', type(idade))
+idade = int(idade)
+print(f'Valor após da conversão: {idade} do tipo ', type(idade))
+
+salario = 3250.85
+print(f'Valor antes da conversão: {salario} do tipo ', type(salario))
+salario = int(salario)
+print(f'Valor antes da conversão: {salario} do tipo ', type(salario))
+
+nota = 7
+print(f'Valor antes da conversão: {nota} do tipo ', type(nota))
+nota = float(nota)
+print(f'Valor antes da conversão: {nota} do tipo ', type(nota))
+
+salario = 3250.85
+print(f'Valor antes da conversão: {salario} do tipo ', type(salario))
+salario = str(salario)
+print(f'Valor antes da conversão: {salario} do tipo ', type(salario))
