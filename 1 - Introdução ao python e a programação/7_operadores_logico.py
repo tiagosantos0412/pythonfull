b15fd9a805208a9c0b3443bd29746890126c6613
Tiago Felipe <tiagosantos0412@gmail.com>
2024-07-28 16:27:56 -0300

    Operadores lógicos.

--- 1 - Introdução ao python e a programação/7_operadores_logico.py
+++ 1 - Introdução ao python e a programação/7_operadores_logico.py
@@ -1,5 +1,18 @@
 # and or not
 
-operador = not ( (5 == 7 or 3 > 2) and  (2 == 2 or 5 < 5) )
- 
+print('Operador and ( E )')
+print(f'True and True = {True and True}')
+print(f'True and False = {True and False}')
+print(f'False and False = {False and False}\n')
+
+print('Operador or ( OU )')
+print(f'True or True = {True or True}')
+print(f'True or False = {True or False}')
+print(f'False or False = {False or False}\n')
+
+print('Operador not ( NÃO )')
+print(f'not True = {not True}')
+print(f'not False = {not False}\n')
+
+operador = 5 == 25 and 10 > 2
 print(operador)
