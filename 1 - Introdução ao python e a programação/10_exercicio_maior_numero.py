65c45f30196ae7a5e706fe87304e0071464eb62a
Tiago Felipe <tiagosantos0412@gmail.com>
2024-07-28 17:41:39 -0300

    Exercício maior número com Estrutura de decisão.

--- 1 - Introdução ao python e a programação/10_exercicio_maior_numero.py
+++ 1 - Introdução ao python e a programação/10_exercicio_maior_numero.py
@@ -4,3 +4,9 @@ n1 = int(input('Digite um número: '))
 n2 = int(input('Digite um número: '))
 n3 = int(input('Digite um número: '))
 
+if n1 > n2 and n2 > n3:
+    print(f'O número maior foi N1 com o valor: {n1}')
+elif n1 < n2 and n2 > n3:
+    print(f'O número maior foi N2 com o valor: {n2}')
+else:
+    print(f'O número maior foi N3 com o valor: {n3}')
