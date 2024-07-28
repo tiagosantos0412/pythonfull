822466c93695a3e9d9a78217a62dd5a084b1d543
Tiago Felipe <tiagosantos0412@gmail.com>
2024-07-28 17:06:29 -0300

    Estrutura de decisão.

--- 1 - Introdução ao python e a programação/8_estrutura_de_decisao.py
+++ 1 - Introdução ao python e a programação/8_estrutura_de_decisao.py
@@ -1,12 +1,17 @@
 # if else elif
-media = 1
+nome_do_aluno = 'Tiago Felipe dos Santos'
+media_escolar = 8
 
-
-if media >= 6:
-    print('APROVADO')
-elif media == 1:
-    print('RECUPERAÇÃO')
-elif media >= 2:
-    print('REPROVADO')
+if media_escolar >= 10:
+    print(f'Parabéns {nome_do_aluno}, você foi aprovado com a nota máxima!')
+    print(f'Sua média foi {media_escolar}')
+elif media_escolar >=7 and media_escolar < 10:
+    print(f'Parabéns {nome_do_aluno}, você foi aprovado.')
+    print(f'Sua média foi {media_escolar}')
+elif media_escolar < 7 and media_escolar >= 5:
+    print(f'Atenção {nome_do_aluno}, você está de recuperação!.')
+    print(f'Sua média foi {media_escolar}')
 else:
-    print('EXPULSO')
+    print(f'Lamento informar {nome_do_aluno}, você foi reprovado.')
+    print(f'Sua média foi {media_escolar}')
+
