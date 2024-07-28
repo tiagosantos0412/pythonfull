400e76b65566fcf3a79709ce88320729aeb31efa
Tiago Felipe <tiagosantos0412@gmail.com>
2024-07-28 16:07:47 -0300

    Operadores Relacionais.

--- 1 - Introdução ao python e a programação/6_operadores_relacional.py
+++ 1 - Introdução ao python e a programação/6_operadores_relacional.py
@@ -1,5 +1,9 @@
 # > < == != <= >=
 
-operador_relacional =  2 <= 3
-
-print(operador_relacional)
+print('Verificação de Operadores Relacionais')
+print(f'8 é maior que 12? {8 > 12}')
+print(f'5 é menor que 22? {5 < 22}')
+print(f'5 é igual a 5? {5 == 5}')
+print(f'15 é diferente de 15? {15 != 15}')
+print(f'12 é maior ou igual a 11? {12 >= 11}')
+print(f'12 é menor ou igual a 11? {12 <= 11}')
