6177c380e9e593fba51b44e2a40e8a1e20f0a722
Tiago <tiagosantos0412@gmail.com>
2024-07-29 14:02:14 -0300

    Exercício masculino ou feminino.

--- 1 - Introdução ao python e a programação/12_exercicio_masculino_feminino.py
+++ 1 - Introdução ao python e a programação/12_exercicio_masculino_feminino.py
@@ -1,3 +1,10 @@
 #Receba F para feminimo e M para masculino e exbia o sexo da pessoa.
 
 sexo = input('Digite F para feminino em M para masculino: ')
+
+if sexo == 'M' or sexo ==  'm':
+    print(f'Usuário do sexo masculino.')
+elif sexo == 'F' or sexo == 'f':
+    print(f'Usuária do sexo feminino.')
+else:
+    print('Valor inválido! Digite F para feminino em M para masculino.')
