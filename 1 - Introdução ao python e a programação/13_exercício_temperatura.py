912f8dd30a186ad12c8387d79614288190190f8c
Tiago <tiagosantos0412@gmail.com>
2024-07-29 15:26:13 -0300

    Exercício converter graus de farenheit para celsius.

--- 1 - Introdução ao python e a programação/13_exercício_temperatura.py
+++ 1 - Introdução ao python e a programação/13_exercício_temperatura.py
@@ -3,6 +3,6 @@
 
 temperatura_farenheit = float(input('Digite uma temperatura em graus farenheit: '))
 
-temperatura_celsius = 5 * ((temperatura_farenheit-32)/9)
+temperatura_celsius = 5 * ((temperatura_farenheit-32) / 9)
 
 print(f'A temperatura em graus celsius é de {temperatura_celsius}')
