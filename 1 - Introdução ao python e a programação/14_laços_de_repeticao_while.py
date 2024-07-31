065deb6a7a5abbf709e6ea5407b64764eef95a05
Tiago <tiagosantos0412@gmail.com>
2024-07-31 11:28:34 -0300

    Laço de repetição while.'

--- 1 - Introdução ao python e a programação/14_laços_de_repeticao_while.py
+++ 1 - Introdução ao python e a programação/14_laços_de_repeticao_while.py
@@ -1,8 +1,14 @@
 i = 0
 
+# while i < 10:
+#     print(i)
+#     i += 1
+#     if i >= 5:
+#         break
+
 while i < 10:
     print(i)
     if i % 2 == 1:
-        i = i + 2
+        i += 2
         continue
-    i = i + 1
+    i += 1
