a3f81d460aac861e6386c7526c3b6a1c2ac5781f
Tiago <tiagosantos0412@gmail.com>
2024-08-05 11:46:20 -0300

    Exercício de login com if e while.

--- 1 - Introdução ao python e a programação/16_exercício_notas_válidas.py
+++ 1 - Introdução ao python e a programação/16_exercício_notas_válidas.py
@@ -2,10 +2,3 @@
 #a nota digitada esteja fora dessa intervalo peça para o professor digitar
 #novamente.
 
-while True:
-    nota_aluno = int(input('Digite a nota do aluno: '))
-    if nota_aluno >= 0 and nota_aluno <= 10:
-        print(f'Nota armazenada com sucesso {nota_aluno}')
-        break
-    
-    print('Nota inválida digite novamente')
--- 1 - Introdução ao python e a programação/17_exercício_login.py
+++ 1 - Introdução ao python e a programação/17_exercício_login.py
@@ -1,15 +1,30 @@
 #Defina um usuário e senha e depois verifique se 
 #login do usuário é valido.
 
-USUARIO = "caio"
-SENHA = "minha_senha123"
+tentativas = 1
+_usuario = 'tiagosantos0412'
+_senha = '123456'
+_nome = 'Tiago Felipe dos Santos'
 
-while True:
-    usuario_login = input('Digite seu nome de usuário: ')
-    senha_login = input('Digite sua senha: ')
+bloqueado = False
+logado = False
 
-    if usuario_login == USUARIO and senha_login == SENHA:
-        print('Você foi logado no sistema!')
+while tentativas <= 3:
+    usuario = input('Usuário...: ')
+    senha = input('Senha.......: ')
+    
+    if usuario == _usuario and senha == _senha:
+        print(f'Bem-vindo {_nome}')
+        logado = True
         break
     else:
-        print('Usuário ou senha inválido.')
+        print('Login inválido!')
+        print('Após a 3ª tentativa seu usuário será bloqueado!')
+        print(f'Tentativas restantes: {3 - tentativas}')
+        logado = False
+    
+    tentativas += 1
+
+if not logado:
+    bloqueado = True
+    print('Usuário bloqueado após 3 tentativas erradas.')
