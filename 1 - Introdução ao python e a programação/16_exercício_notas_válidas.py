6d5779b49175937fe86c4be314bfe8613f2361c3
Tiago <tiagosantos0412@gmail.com>
2024-08-05 13:02:12 -0300

    Exercício de notas válidas com while e if.

--- 1 - Introdução ao python e a programação/16_exercício_notas_válidas.py
+++ 1 - Introdução ao python e a programação/16_exercício_notas_válidas.py
@@ -2,3 +2,19 @@
 #a nota digitada esteja fora dessa intervalo peça para o professor digitar
 #novamente.
 
+controlador = True
+contador = 0
+notas = []
+
+
+while controlador:
+    nota_digitada = int(input('Digite a nota:'))
+    if nota_digitada < 0 or nota_digitada > 10:
+        print('Erro no intervalo de notas!\nDigite as notas entre 0 e 10!')
+    else:
+        notas.append(nota_digitada)
+        contador += 1
+    if contador >= 4:
+        controlador = False
+    
+print(f'Notas registradas.....: {notas}')
