b5d6d0da4ebd1641ac44a95c5a21d821f9142ff7
Tiago <tiagosantos0412@gmail.com>
2024-08-06 16:27:31 -0300

    Exercício de tabuada com for

--- 1 - Introdução ao python e a programação/20_exercicio_tabuada_for.py
+++ 1 - Introdução ao python e a programação/20_exercicio_tabuada_for.py
@@ -1,6 +1,18 @@
 #Receba um número e mostra a tabuada completa dele usando o laço for.
 
-n1 = int(input('Digite um número: '))
+# numero = int(input('Escolha um número para calcularmos a tabuada.....: '))
 
-for i in range(1, 11):
-    print(f'{n1} x {i} = {n1*i}')
+# for i in range(0, 11):
+#     print(f'{numero} X {i} = {numero * i}')
+
+for i in range(0, 11):
+    print(f'{i} X {1} = {1 * i}')
+    print(f'{i} X {2} = {2 * i}')
+    print(f'{i} X {3} = {3 * i}')
+    print(f'{i} X {4} = {4 * i}')
+    print(f'{i} X {5} = {5 * i}')
+    print(f'{i} X {6} = {6 * i}')
+    print(f'{i} X {7} = {7 * i}')
+    print(f'{i} X {8} = {8 * i}')
+    print(f'{i} X {9} = {9 * i}')
+    print(f'{i} X {10} = {10 * i}')
---
+++ 1 - Introdução ao python e a programação/tempCodeRunnerFile.py
@@ -0,0 +1,4 @@
+numero = int(input('Escolha um número para calcularmos a tabuada.....: '))
+
+# for i in range(0, 11):
+#     print(f'{numero} X {i} = {numero * i}')
