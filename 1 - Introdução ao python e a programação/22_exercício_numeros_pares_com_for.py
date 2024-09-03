2b4ddf55ab946705e657a50302a8d2b0c801b1a7
Tiago <tiagosantos0412@gmail.com>
2024-09-03 18:18:47 -0300

    Exercicíco números pares com for.

--- 1 - Introdução ao python e a programação/22_exercício_numeros_pares_com_for.py
+++ 1 - Introdução ao python e a programação/22_exercício_numeros_pares_com_for.py
@@ -1,4 +1,8 @@
 #Exiba todos os números pares entre 1 e 1000 usando o for.
 
-for i in range(2, 1001, 2):
+# for i in range(1, 1001):
+#     if i % 2 == 0:
+#         print(i)
+
+for i in range(0, 1001, 2):
     print(i)
