b1cf9e7bfad42123faff1a19d21059941214edb4
Tiago <tiagosantos0412@gmail.com>
2024-09-05 18:29:43 -0300

    Métodos para lista, descobrindo o índice de um elemnto na lista com o index.

---
+++ 1 - Introdução ao python e a programação/23_2_listas-3.py
@@ -0,0 +1,4 @@
+produto = ['Notebook', 'Monitor LG', 'Teclado Logitec', 'Processo Risen']
+# Descobrindo o index de um elemnto de uma lista
+indice = produto.index('Teclado Logitec')
+print(f'O índice do produto Teclado LOgitec é: {indice}')
