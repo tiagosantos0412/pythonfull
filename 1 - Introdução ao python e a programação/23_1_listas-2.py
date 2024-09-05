2b5233780b3ce34d4a0070bb8e86ed7d47929ccb
Tiago <tiagosantos0412@gmail.com>
2024-09-05 17:53:39 -0300

    Métodos para lista remoção de dados com o pop.

--- 1 - Introdução ao python e a programação/23_1_listas-2.py
+++ 1 - Introdução ao python e a programação/23_1_listas-2.py
@@ -7,5 +7,13 @@ idades = [37, 25, 70, 49]
 idades.insert(0, 22) # adicionando o ítem 22 no índice 0
 idades.insert(2, 58) # adicionando o ítem 58 no índice 2
 
+print(idades)
 
+# Remover ítens da lista com o pop
+idades.pop() # removido o último numero da lista
+print(idades)
+
+# posso também informar um index existente no range da lista para ser removido
+# removendo o ítem do índice 0
+idades.pop(0)
 print(idades)
