938774485277076d674c8ac7c629f9b2e0ae5ff2
Tiago <tiagosantos0412@gmail.com>
2024-09-05 19:27:01 -0300

    Métodos para lista, utilizando o método sort e sorted para ordenar listas.

--- 1 - Introdução ao python e a programação/23_2_listas-3.py
+++ 1 - Introdução ao python e a programação/23_2_listas-3.py
@@ -1,4 +1,24 @@
-produto = ['Notebook', 'Monitor LG', 'Teclado Logitec', 'Processo Risen']
+produto = ['Notebook', 'Monitor LG', 'Teclado Logitec', 'Processo Risen', 'Adaptador USB']
 # Descobrindo o index de um elemnto de uma lista
 indice = produto.index('Teclado Logitec')
 print(f'O índice do produto Teclado LOgitec é: {indice}')
+
+# Podemos utilizar o método sort para ordenar uma lista
+# o comando sort altera a lista original
+produto.sort() # ordenando os ítens em ordem alfabética
+print(produto)
+
+numeros = [5, 12, 7, 1, 9, 3]
+numeros.sort() #ordenando os números em ordem crescente
+print(numeros)
+
+numeros.sort(reverse=True) #ordenando os números em ordem decrescente
+print(numeros)
+
+# Se não quisermos alterar a lista original, podemos utilizar
+# o método sorted, que faz uma cópia da lista original e altera apenas a cópia.
+print(sorted(numeros))
+# Podemos também armazenas esta cópia da lista em uma nova variável
+numeros_ordenados = sorted(numeros)
+print(numeros_ordenados)
+
