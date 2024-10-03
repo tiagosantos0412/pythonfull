3d64ad55d66b362d8f8e22fa199a3e9e65aa8753
Tiago <tiagosantos0412@gmail.com>
2024-10-03 12:28:34 -0300

    List_comprehension

--- 1 - Introdução ao python e a programação/24_List_comprehension.py
+++ 1 - Introdução ao python e a programação/24_List_comprehension.py
@@ -1,2 +1,12 @@
-x = [1, 2, 3, 4]
+x = [i for i in range(0, 10)]
 
+print(x)
+
+nomes = [input('Digite um nome...: ') for i in range(0, 3)]
+print(nomes)
+
+y = [[j for j in range(3, 7)] for i in range(0, 3)]
+print(y)
+
+k = [i for i in range(0, 11) if i > 4]
+print(k)
